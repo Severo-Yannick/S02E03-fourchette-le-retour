145921d778d30ba1091dcdc9cf7012c180c44142
Yannick Severo <severoyannick@gmail.com>
2021-09-17 15:55:38 +0200

    Créer un objet de configuration

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
-// Le nombre max
-var max = 500;
+var game = {
+  // Le nombre max
+  max: 500,
+  // Le nombre d'essais
+  attemps:0,
+  // Le nombre cherché
+  searchedNumber: null,
+}
 
 // Le nombre cherché
-var searchedNumber = Math.round(Math.random() * max);
+game.searchedNumber = Math.round(Math.random() * game.max);
 
 // Le nombre saisi
 var enteredNumber = parseInt(prompt("Quel est le nombre à trouver ?"));
 
-// Le nombre d'essais
-var attemps = 1;
-
 // Tant que le nombre saisi n'est pas bon on redemande un nombre
-while (enteredNumber !== searchedNumber) {
+while (enteredNumber !== game.searchedNumber) {
   // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
-  if (enteredNumber < searchedNumber) {
+  if (enteredNumber < game.searchedNumber) {
     enteredNumber = parseInt(prompt("C'est plus"));
   } else {
     enteredNumber = parseInt(prompt("C'est moins"));
   }
   // on incrémente le nombre d'essais
-  attemps += 1;
+  game.attemps += 1;
 }
 
 // on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
 // on affiche un message de victoire
 alert(
-  "Bravo ! C'était bien " + searchedNumber + " - Nombre d'essais : " + attemps
+  "Bravo ! C'était bien " + game.searchedNumber + " - Nombre d'essais : " + game.attemps
 );
