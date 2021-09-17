dc78514a9e642cea697995d7f04f6e7cf99a7803
Yannick Severo <severoyannick@gmail.com>
2021-09-17 16:05:21 +0200

    Faire une fonction pour jouer

--- js/main.js
+++ js/main.js
@@ -9,24 +9,29 @@ var game = {
 
 // Le nombre cherché
 game.searchedNumber = Math.round(Math.random() * game.max);
+console.log(game.searchedNumber);
 
 // Le nombre saisi
 var enteredNumber = parseInt(prompt("Quel est le nombre à trouver ?"));
 
-// Tant que le nombre saisi n'est pas bon on redemande un nombre
-while (enteredNumber !== game.searchedNumber) {
-  // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
-  if (enteredNumber < game.searchedNumber) {
-    enteredNumber = parseInt(prompt("C'est plus"));
-  } else {
-    enteredNumber = parseInt(prompt("C'est moins"));
+function play() {
+  // Tant que le nombre saisi n'est pas bon on redemande un nombre
+  while (enteredNumber !== game.searchedNumber) {
+    // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
+    if (enteredNumber < game.searchedNumber) {
+      enteredNumber = parseInt(prompt("C'est plus"));
+    } else {
+      enteredNumber = parseInt(prompt("C'est moins"));
+    }
+    // on incrémente le nombre d'essais
+    game.attemps += 1;
   }
-  // on incrémente le nombre d'essais
-  game.attemps += 1;
+  
+  // on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
+  // on affiche un message de victoire
+  alert(
+    "Bravo ! C'était bien " + game.searchedNumber + " - Nombre d'essais : " + game.attemps
+  );
 }
 
-// on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
-// on affiche un message de victoire
-alert(
-  "Bravo ! C'était bien " + game.searchedNumber + " - Nombre d'essais : " + game.attemps
-);
+play();
